d6f1f197be059302211058c77dc004495a27e013
Miaad <Miaad@MIAAD>
2024-12-22 15:27:09 +0330

    Enhance main.py to include action names for optimal policy display and adjust environment settings for Monte Carlo and Q-Learning algorithms

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from value_iteration import ValueIteration
 from policy_iteration import PolicyIteration
 from monte_carlo import MonteCarlo
 from q_learning import QLearning
+import numpy as np
 
 def get_algorithm_choice():
     while True:
@@ -17,23 +18,42 @@ def main():
     env = FrozenLake(render_mode="human", map_name="8x8")
     observation, info = env.reset(seed=30)
     terminal_state = env.nS - 1
+    action_names = {0: 'UP', 1: 'RIGHT', 2: 'DOWN', 3: 'LEFT'}
     
-    # Initialize chosen algorithm
     if choice == '1':
         print("\nUsing Value Iteration")
         solution = ValueIteration(env, terminal_state=terminal_state)
+        
     elif choice == '2':
         print("\nUsing Policy Iteration")
         solution = PolicyIteration(env, terminal_state=terminal_state)
+        
     elif choice == '3':
         print("\nUsing Monte Carlo")
+        env.update_speed(2)
+        env.render_mode = "ansi"
         solution = MonteCarlo(env, terminal_state=terminal_state)
+        
     elif choice == '4':
         print("\nUsing Q-Learning")
+        env.update_speed(0.8)
+        env.render_mode = "ansi"
         solution = QLearning(env, terminal_state=terminal_state)
+
     
     policy = solution.solve()
+    
+    policy_grid = np.array(policy).reshape(8, 8)
+    policy_grid = np.vectorize(action_names.get)(policy_grid)
+    
+    print("Optimal Policy:")
+    for row in policy_grid:
+        print(" ".join(f"{action:5}" for action in row))
+        
+        
     current_state = observation
+    
+    env.render_mode = "human"
 
     for __ in range(max_iter_number):
         if current_state == terminal_state:
